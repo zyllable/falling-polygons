f760a0759fc6c3d886dcfed971bcdddd4277605b
zyllable <57426872+zyllable@users.noreply.github.com>
2023-11-13 14:42:09 -0600

    added controls, rotate no work

--- zetris/game.js
+++ zetris/game.js
@@ -18,12 +18,15 @@ const blocks = [ //block notation: width, height, t/f for each from top left to 
 	[3, 2, true, true, false, false, true, true],
 	[3, 2, false, true, false, true, true, true],
 ];
-let columns
 const main = () => {
 	let canvas = document.querySelector("#gameTarget");
 	let ctx = canvas.getContext("2d");
 	const bWidth = 10
 	const bHeight = 21
+	let rotations = 0;
+	let width = 0;
+	let height = 0;
+
 
 	ctx.fillStyle = "green"
 
@@ -32,7 +35,7 @@ const main = () => {
 	}
 	const startGame = () => {
 		//setup columns
-		columns = new Array(bWidth).fill(false,0,bWidth);
+		let columns = new Array(bWidth).fill(false,0,bWidth);
 		for (let column in columns) {
 			column = Number(column);
 			columns[column] = new Array(bHeight).fill(false,0,bHeight)
@@ -62,13 +65,11 @@ const main = () => {
 		}
 		const newBlock = () => {
 			const newBlock = randomIndex(blocks);
-			const width = newBlock[0];
-			//const height = newBlock[1];
-			console.log(movingBlocks)
+			width = newBlock[0];
+			height = newBlock[1];
 			for (let block in newBlock) {
 				block = Number(block);
 				if (block > 1) {
-					console.log((bWidth / 2), Math.ceil(width / 2), (block - 2) % width)
 					if(newBlock[block]) {/*
 					ALGORITHM???
 					y: (block / width to floor
@@ -81,7 +82,6 @@ const main = () => {
 					}
 				}
 			}
-			console.log(movingBlocks)
 		}
 		const doLines = () => {
 			//clearing lines and doing scores
@@ -95,7 +95,103 @@ const main = () => {
 			doLines();
 			newBlock();
 		}
-		const rotate = () => {
+		const rotate = (dir) => {
+			//counter clockwise: (quadrant 1-4)
+			//(+, +), (-, +), (-, -), (+, -)
+			//follows x,y,x,y pattern of negatives
+			//clockwise is the same but reverse
+
+			//true = clockwise
+			//false = counter-clockwise
+			if (dir) {
+				//returns are not to be used its just so i can break the function as a whole
+				rotations += 1;
+				//this ones gonna take a lot more work
+				/*
+				HOW TO ROTATE???
+				so if its an odd rotation then invert the y, if its an even rotation invert the x
+				----
+				. new moving blocks is cropped moving blocks
+					. to crop find leftmost piece and get the other side with width, then find topmost piece and get the other side with height
+				. find middle of new moving blocks, rounded to left (down i guess) and top (down probably) no rounding necessary if its these
+				. swap y and x around the new axes
+				. swap width and height
+				*/
+				let leftMost = bWidth;
+				let topMost = bHeight;
+				//get edges of blocks
+				for (let column in movingBlocks) {
+					column = Number(column);
+					for (let cell in movingBlocks[column]) {
+						cell = Number(cell);
+						if (movingBlocks[column][cell]) {
+							if (column < leftMost) {leftMost = column}
+							if (cell < topMost) {topMost = cell}
+						}
+					}
+				}
+				console.log(leftMost, topMost)
+				//get cropped array
+				let movingBlocks2 = []
+				for (let i = 0; i < height; i++) { //x and y are swapped
+					movingBlocks2[i] = [];
+					for (let j = 0; j < width; j++) {
+						movingBlocks2[i][j] = movingBlocks[leftMost + j][topMost + i];
+					}
+				}
+
+				//if odd invert y, if even invert x
+				let movingBlocks3 = new Array(height).fill(false,0,movingBlocks2.height);
+				for (let column in movingBlocks3) {
+					column = Number(column);
+					movingBlocks3[column] = new Array(width).fill(false,0,movingBlocks2[0].width);
+				}
+				console.log(movingBlocks2, movingBlocks3)
+				if (rotations % 2 == 1) {
+					for (let column in movingBlocks2) {
+						column = Number(column);
+
+						for (let cell in movingBlocks2[column]) {
+							cell = Number(cell);
+
+							movingBlocks3[column][movingBlocks2[0].length - 1 - cell] = movingBlocks2[column][cell];
+						}
+					}
+				} else {
+					for (let column in movingBlocks2) {
+						column = Number(column);
+
+						for (let cell in movingBlocks2[column]) {
+							cell = Number(cell);
+
+							movingBlocks3[movingBlocks2.length - 1 - column][cell] = movingBlocks2[column][cell];
+						}
+					}
+				}
+
+				//check for collisions (less work than moving i think)
+				//also cement the stuff into the columns thing
+				//if new thing will be occupied, if not moving, if already occupied
+				for (let column in movingBlocks3) {
+					column = Number(column);
+
+					for (let cell in movingBlocks3[column]) {
+						if (movingBlocks3[column][cell] && !movingBlocks[leftMost + column][topMost + cell] && columns[leftMost + column][topMost + cell]) {
+							return false
+						} else {
+							columns[leftMost + column][topMost + cell] = movingBlocks3[column][cell];
+						}
+					}
+				}	
+				const tempwidth = width;
+				width = height;
+				height = tempwidth;
+				render();
+				return true;
+			} else {
+				rotations -= 1;
+			}
+
 			render();
 		}
 		const collisionDetector = (dir) => {
@@ -149,7 +245,7 @@ const main = () => {
 						for (let block in movingBlocks[column]) {
 							block = Number(block);
 							if (movingBlocks[column][block]) {
-								if (column >= bWidth) {
+								if (column >= bWidth - 1) {
 									return true;
 								}
 								if (!movingBlocks[column + 1][block]) {
@@ -218,11 +314,77 @@ const main = () => {
 		const horizontalMove = (dir) => {
 			//dir = true is right, false is left
 			if (dir) {
-				if (!collisionDetector(1)) {
+				if (!collisionDetector(2)) {
+					let movingBlocks2 = new Array(bWidth).fill(false,0,bWidth);
+
+					for (let column in movingBlocks2) {
+						column = Number(column);
+						movingBlocks2[column] = new Array(bHeight).fill(false,0,bHeight)
+					}
+
+					//this whole damn thing is because arrays work on references therefore have to be algorithmically cloned
+					for (let column in movingBlocks) {
+						column = Number(column);
+						for (let row in movingBlocks[column]) {
+							row = Number(row)
+							if (movingBlocks[column][row]) {
+								//moving blocks into new array
+								movingBlocks2[column + 1][row] = movingBlocks[column][row];
+
+								//remove moving blocks from columns
+								columns[column][row] = false;
+							}
+						}
+					}
+					for (let column in movingBlocks2) {
+						column = Number(column);
+						for (let row in movingBlocks2[column]) {
+							row = Number(row)
+
+							//put new moving blocks onto columns
+							if (movingBlocks2[column][row]) {
+								columns[column][row] = movingBlocks2[column][row];
+							}
+						}
+					}
+					movingBlocks = movingBlocks2;
 					render();
 				}
 			} else {
-				if (!collisionDetector(2)) {
+				if (!collisionDetector(1)) {
+					let movingBlocks2 = new Array(bWidth).fill(false,0,bWidth);
+
+					for (let column in movingBlocks2) {
+						column = Number(column);
+						movingBlocks2[column] = new Array(bHeight).fill(false,0,bHeight)
+					}
+
+					//this whole damn thing is because arrays work on references therefore have to be algorithmically cloned
+					for (let column in movingBlocks) {
+						column = Number(column);
+						for (let row in movingBlocks[column]) {
+							row = Number(row)
+							if (movingBlocks[column][row]) {
+								//moving blocks into new array
+								movingBlocks2[column - 1][row] = movingBlocks[column][row];
+
+								//remove moving blocks from columns
+								columns[column][row] = false;
+							}
+						}
+					}
+					for (let column in movingBlocks2) {
+						column = Number(column);
+						for (let row in movingBlocks2[column]) {
+							row = Number(row)
+
+							//put new moving blocks onto columns
+							if (movingBlocks2[column][row]) {
+								columns[column][row] = movingBlocks2[column][row];
+							}
+						}
+					}
+					movingBlocks = movingBlocks2;
 					render();
 				}
 			}
@@ -230,7 +392,37 @@ const main = () => {
 		const endGame = () => {
 
 		}
+		document.addEventListener("keydown", (e) => {
+			switch (e.code) {
+				case "KeyA": //a
+				case "ArrowLeft": //left
+				horizontalMove(false);
+				break;
 
+				case "KeyD": //d
+				case "ArrowRight": //right
+				horizontalMove(true);
+				break;
+
+				case "KeyS": //s
+				case "ArrowDown": //down
+				gameTick();
+				break;
+
+				case "Space": //space
+				while (!gameTick()) {}
+
+				case "KeyX": //x
+				case "Period": //period
+				rotate(true);
+				break;
+
+				case "KeyZ": //z
+				case "Comma": //comma
+				rotate(false);
+				break;
+			}
+		})
 		newBlock();
 		render();
 		let ticks = setInterval(gameTick, 1000)
